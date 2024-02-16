7e61c685c7c50a984d4366b97ac1514fce30353e
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-02-16 18:44:07 +0530

    Add files via upload

---
+++ DataTypes3.java
@@ -0,0 +1,8 @@
+class DataTypes3{
+        public static void main(String[] args){
+                long distance=2023974774;
+                System.out.println("Distance = "+distance);
+        }
+
+
+}
