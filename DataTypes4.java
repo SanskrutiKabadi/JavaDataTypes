2b07cb2c2faf6cbcd73ce9b1d6c5cf17fbabe426
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-02-16 18:44:29 +0530

    Add files via upload

---
+++ DataTypes4.java
@@ -0,0 +1,8 @@
+class DataTypes4{
+        public static void main(String[] args){
+                double distance=20.2397477785784;
+                System.out.println("Distance = "+distance);
+        }
+
+
+}
