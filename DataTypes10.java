fd41d6162d980360e0a9c4dd50e997cb4d760344
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-02-18 22:04:55 +0530

    Add files via upload

---
+++ DataTypes10.java
@@ -0,0 +1,11 @@
+class DataTypes10{
+        public static void main(String[] args){
+                int a=678909;
+		float b=a;
+                System.out.println("a= "+a);
+		System.out.println("b= "+b);
+
+        }
+
+
+}
