438f0e1c4d1900c6a642b424ce054de3b147e1b9
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-12 07:50:38 +0530

    Java Data Types

--- DataTypes10.java
+++ DataTypes10.java
@@ -1,7 +1,8 @@
 class DataTypes10{
         public static void main(String[] args){
-                int a=678909;
-		float b=a;
+                int a=678909;//4 bytes
+		
+		float b=a; //4 bytes
                 System.out.println("a= "+a);
 		System.out.println("b= "+b);
 
