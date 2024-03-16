22dad7554f420f511dbe2c3e5d2f26c60c18b14e
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-16 08:07:49 +0530

    Float Data Type

--- DataTypes2.java
+++ DataTypes2.java
@@ -1,5 +1,6 @@
 class DataTypes2{
         public static void main(String[] args){
+                
                 float salary=2023974.7f;
                 System.out.println("Salary = "+salary);
         }
