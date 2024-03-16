a5a89237a9e041216cb0baf0c9bebd7e37832b3d
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-16 08:09:27 +0530

    Long Data Type

--- DataTypes3.java
+++ DataTypes3.java
@@ -1,5 +1,6 @@
 class DataTypes3{
         public static void main(String[] args){
+                
                 long distance=2023974774;
                 System.out.println("Distance = "+distance);
         }
