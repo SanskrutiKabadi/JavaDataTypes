3bdd05697a70459bc211818ddae18633bffcb125
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-22 21:05:45 +0530

    Double

--- DataTypes4.java
+++ DataTypes4.java
@@ -1,6 +1,7 @@
 class DataTypes4{
         public static void main(String[] args){
                 double distance=20.2397477785784;
+                
                 System.out.println("Distance = "+distance);
         }
 
